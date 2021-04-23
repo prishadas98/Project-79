b9759848d4f0df81c9c8308f766750fbbac35480
prishadas98 <79792535+prishadas98@users.noreply.github.com>
2021-04-23 09:49:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+menu_list_array=["Chicken Tandoori Pizza",
+                  "Chicken Ranch Pizza",
+                  "Hot chicken Pizza",
+                  "Veg Supreme Pizza"];
+
+function getmenu(){
+    
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(var i=0;i<menu_list_array.length;i++)
+    {
+        htmldata=htmldata+'<li>'+ menu_list_array[i]+'</li>'
+
+    }
+    htmldata=htmldata+"</ol>"
+    document.getElementById("display_menu").innerHTML=htmldata;
+}
+function added_item() {
+    var htmldata;
+    var item=document.getElementById("added_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    for(var i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<div class="card">'+ '<img src="images/pizzaImg.png"/>'+menu_list_array[i]+'</div'
+    }
+    htmldata=htmldata+"</section>"
+    document.getElementById("display_addedmenu").innerHTML=htmldata;
+    
+}
